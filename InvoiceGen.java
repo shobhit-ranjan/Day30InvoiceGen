e91117d1a1ea5ffa178c3febc719c654da62d85a
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-27 15:02:34 +0530

    UC1 fare Cal

--- InvoiceGen.java
+++ InvoiceGen.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 
 public class InvoiceGen {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Invoice Gen.");
+	static int dis;
+	static int time;
+
+	static void getridedetails() {
+		Invoice ride = new Invoice();
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter the distance ");
+		dis = sc.nextInt();
+		//ride.setDistance(sc.next());
+		System.out.println(" Enter the Time in min ");
+		time = sc.nextInt();
+		//ride.setTime(sc.next());
+		sc.close();
+
+	}
+
+	static void billGenerater() {
+		int totalcost = (dis * 10) + time;
+		
+		if(totalcost<5)
+		{
+			System.out.println("for today you have pay 5$ as a base fare");
+		}
+		else
+		{
+			System.out.println("for today you have pay " + totalcost + " $ as you fare");
+		}
+		
+		
+		
 	}
 }
