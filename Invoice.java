774d4ff01fe25e720b35bd83048826f23412f371
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-27 14:35:37 +0530

    first commit

---
+++ Invoice.java
@@ -0,0 +1,22 @@
+
+public class Invoice {
+
+	private int distance;
+	private int time;
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setLastName(int time) {
+		this.time = time;
+	}
+}
---
+++ InvoiceGen.java
@@ -0,0 +1,6 @@
+
+public class InvoiceGen {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Invoice Gen.");
+	}
+}
